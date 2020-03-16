35d886537cd5bff2ee9aaa3b3639ebf43907793c
unknown <jeffrey.d.helder@gmail.com>
2020-03-16 11:09:46 -0400

    added some more sections to edit-site page.

--- admin/edit_site.php
+++ admin/edit_site.php
@@ -19,7 +19,7 @@
 <?php include_once 'templates/header.php'; ?>
     <main>
         <div class="title">
-            <h1>Site Content Management</h1>
+            <h1>RRY - Content Management System</h1>
             <p><i>Please be aware of image size restrictions as well as <b>Web and Mobile</b> sizing.</i></p>
         </div>
         <div class="site-nav">
@@ -37,7 +37,7 @@
         </div>
         <div id="hero">
             <h1>Hero Section</h1>
-            <h2>Section Images</h2>
+            <h2>Hero-Section Image(s)</h2>
             <div class="images">
                 <div class="image-container">
                     <div class="image">
@@ -62,27 +62,24 @@
                     </form>
                 </div>
             </div>
-            <h2>Section Copy</h2>
-            <div>
-                <h3>Courage Makes Change</h3>
+            <h2>Hero-Section Copy</h2>
+            <div class="copy">
+                <div class="copy-container">
+                    <div class="text">
+                        <p>" Courage Makes Change "</p>
+                    </div>
+                    <h2>Hero-Tag-Line</h2>
+                    <form>
+				        <label for="message">New Content</label>
+				        <textarea class="message" type="text" autocomplete="off" id="textarea"></textarea>
+				        <input type="submit">
+			        </form>
+                </div> 
             </div>
         </div>
         <div id="about">
             <h1>About-Us Section</h1>
-            <h2>Section Copy</h2>
-            <div class="text">
-                <h3>About Us</h3>
-                <h4>
-                    The stigma is a result of misconceptions and it needs to end. Red Ribbon Youth is 
-                    dedicated to combat the HIV discrimination through raising awareness and understanding 
-                    among teenagers.
-                </h4>
-            </div>
-            <h2>Section Video</h2>
-            <div class="video">
-                <video src="../"></video>
-            </div>
-            <h2>Section Images</h2>
+            <h2>About-Section Image(s)</h2>
             <div class="images">
                 <div class="image-container">
                     <div class="image">
@@ -96,24 +93,246 @@
                     </form>
                 </div> 
             </div>
+            <h2>About-Section Video(s)</h2>
+            <div class="video">
+                <div class="video-container">
+                    <div class="video">
+                        <video src="../"></video>
+                    </div>
+                    <h2>About-Video</h2>
+                    <form>
+				        <input type="file">
+			        </form>
+                </div> 
+            </div>
+            <h2>About-Section Copy</h2>
+            <div class="copy">
+                <div class="copy-container">
+                    <div class="text">
+                        <p>"
+                            About Us 
+                        "</p>
+                    </div>
+                    <h2>About-Title</h2>
+                    <form>
+				        <label for="message">New Content</label>
+				        <textarea class="message" type="text" autocomplete="off" id="textarea"></textarea>
+				        <input type="submit">
+			        </form>
+                </div> 
+                <div class="copy-container">
+                    <div class="text">
+                        <p>"
+                            The stigma is a result of misconceptions and it needs to end. Red Ribbon Youth is 
+                            dedicated to combat the HIV discrimination through raising awareness and understanding 
+                            among teenagers. 
+                        "</p>
+                    </div>
+                    <h2>About-Description</h2>
+                    <form>
+				        <label for="message">New Content</label>
+				        <textarea class="message" type="text" autocomplete="off" id="textarea"></textarea>
+				        <input type="submit">
+			        </form>
+                </div> 
+            </div>
         </div>
+
         <div id="workshop">
 
         </div>
+
         <div id="why">
-
+            <h1>Why Section</h1>
+            <h2>Why Image(s)</h2>
+            <div class="images">
+                <div class="image-container">
+                    <div class="image">
+                        <img class="about-back" src="../images/why_background.jpg" alt="why background">
+                    </div>
+                    <h2>Why-Background</h2>
+                    <p><i><b>Maximum file size:</b> 500kb</i></p>    
+                    <p><i><b>Suggested dimensions:</b> 1437x724</i></p>
+                    <form action="">
+                        <input type="file">
+                    </form>
+                </div> 
+            </div>
         </div>
+
         <div id="how">
-
+            <h1>How Section</h1>
+            <h2>How-Box-One</h2>
+            <div class="copy">
+                <div class="copy-container">
+                    <div class="text">
+                        <p>"
+                            Anonymous Testing 
+                        "</p>
+                    </div>
+                    <h2>Box-Title</h2>
+                    <form>
+				        <label for="message">New Content</label>
+				        <textarea class="message" type="text" autocomplete="off" id="textarea"></textarea>
+				        <input type="submit">
+			        </form>
+                </div> 
+                <div class="copy-container">
+                    <div class="text">
+                        <p>"
+                            An anonymous test doesn't require your name or health card. 
+                            The test is ordered using a six digit code instead of your name. 
+                            Typically, anonymous tests are also rapid tests, which are done 
+                            on-site by sampling blood from a quick finger prick. You receive 
+                            the results in about 20 minutes.
+                        "</p>
+                    </div>
+                    <h2>Box-Paragraph</h2>
+                    <form>
+				        <label for="message">New Content</label>
+				        <textarea class="message" type="text" autocomplete="off" id="textarea"></textarea>
+				        <input type="submit">
+			        </form>
+                </div> 
+            </div>
+            <h2>How-Box-Two</h2>
+            <div class="copy">
+                <div class="copy-container">
+                    <div class="text">
+                        <p>"
+                            Confidential Testing 
+                        "</p>
+                    </div>
+                    <h2>Box-Title</h2>
+                    <form>
+				        <label for="message">New Content</label>
+				        <textarea class="message" type="text" autocomplete="off" id="textarea"></textarea>
+				        <input type="submit">
+			        </form>
+                </div> 
+                <div class="copy-container">
+                    <div class="text">
+                        <p>"
+                            A confidential test, like the one you would get at a heath unit 
+                            or doctor's office, uses your name and usually involves a 
+                            standard blood draw. The results take longer to come back than 
+                            rapid test results because they are sent to a lab.
+                        "</p>
+                    </div>
+                    <h2>Box-Paragraph</h2>
+                    <form>
+				        <label for="message">New Content</label>
+				        <textarea class="message" type="text" autocomplete="off" id="textarea"></textarea>
+				        <input type="submit">
+			        </form>
+                </div> 
+            </div>
         </div>
+
         <div id="where">
 
         </div>
         <div id="join">
-
+        <h1>Join-Us Section</h1>
+        <h2>Join-Section Copy</h2>
+            <div class="copy">
+                <div class="copy-container">
+                    <div class="text">
+                        <p>"
+                            Join Us On Instagram
+                        "</p>
+                    </div>
+                    <h2>Join-Title</h2>
+                    <form>
+				        <label for="message">New Content</label>
+				        <textarea class="message" type="text" autocomplete="off" id="textarea"></textarea>
+				        <input type="submit">
+			        </form>
+                </div> 
+                <div class="copy-container">
+                    <div class="text">
+                        <p>"
+                        Pass along your courage with #redribbonyouth. Be brave enough to say 
+                        no to unprotected sex, to say yes to get tested. And inspired others 
+                        with your story.
+                        "</p>
+                    </div>
+                    <h2>Join-Description</h2>
+                    <form>
+				        <label for="message">New Content</label>
+				        <textarea class="message" type="text" autocomplete="off" id="textarea"></textarea>
+				        <input type="submit">
+			        </form>
+                </div> 
+            </div>
         </div>
         <div id="footer">
-
+            <h1>Footer Section</h1>
+            <h2>Newsletter</h2>
+            <div class="copy">
+                <div class="copy-container">
+                    <div class="text">
+                        <p>"
+                            Sign Up For Our Newsletter 
+                        "</p>
+                    </div>
+                    <h2>Title</h2>
+                    <form>
+				        <label for="message">New Content</label>
+				        <textarea class="message" type="text" autocomplete="off" id="textarea"></textarea>
+				        <input type="submit">
+			        </form>
+                </div> 
+                <div class="copy-container">
+                    <div class="text">
+                        <p>"
+                            We will keep you posted with our latest workshop and 
+                            testing information
+                        "</p>
+                    </div>
+                    <h2>Description</h2>
+                    <form>
+				        <label for="message">New Content</label>
+				        <textarea class="message" type="text" autocomplete="off" id="textarea"></textarea>
+				        <input type="submit">
+			        </form>
+                </div> 
+            </div>
+            <h2>Footer-Copy</h2>
+            <div class="copy">
+                <div class="copy-container">
+                    <div class="text">
+                        <p>"
+                            Red Ribbon Youth is dedicated to combat the HIV discrimination 
+                            through raising awareness and understanding among teenagers. 
+                            Information on this site is not to be used for diagnosis, treatment 
+                            or referral services. Individuals should contact their personal
+                            physician, and/or their local addiction or mental health agency 
+                            for further information. If you are in crisis, please go to your 
+                            local hospital or call 911 immediately.
+                        "</p>
+                    </div>
+                    <h2>Footer-Note</h2>
+                    <form>
+				        <label for="message">New Content</label>
+				        <textarea class="message" type="text" autocomplete="off" id="textarea"></textarea>
+				        <input type="submit">
+			        </form>
+                </div> 
+                <div class="copy-container">
+                    <div class="text">
+                        <p>"
+                            © Red Ribbon Youth, 2020. All rights reserved.
+                        "</p>
+                    </div>
+                    <h2>Copywrite</h2>
+                    <form>
+				        <label for="message">New Content</label>
+				        <textarea class="message" type="text" autocomplete="off" id="textarea"></textarea>
+				        <input type="submit">
+			        </form>
+                </div>
+            </div>
         </div>
     </main>
 </body>
